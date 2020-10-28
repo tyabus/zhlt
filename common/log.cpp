3d96c7a45023d38f3cb692fbb94eeb94364650ff
tyabus <tyabustest@gmail.com>
2020-10-28 12:04:51 +0600

    Remove unused defines

--- common/log.cpp
+++ common/log.cpp
@@ -283,22 +283,6 @@ void CDECL      Error(const char* const error, ...)
     char            message[MAX_ERROR];
     char            message2[MAX_ERROR];
     va_list         argptr;
-    
- /*#if defined( SYSTEM_WIN32 ) && !defined( __MINGW32__ ) && !defined( __BORLANDC__ )
-    {
-        char* wantint3 = getenv("WANTINT3");
-		if (wantint3)
-		{
-			if (atoi(wantint3))
-			{
-				__asm
-				{
-					int 3;
-				}
-			}
-		}
-    }
-#endif*/
 
     va_start(argptr, error);
     vsnprintf(message, MAX_ERROR, error, argptr);
